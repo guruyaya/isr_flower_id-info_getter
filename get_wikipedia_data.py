5304c27986feae22cb919392e7fae645349c7dd5
Yair Eshel Cahansky <guruyaya@gmail.com>
2020-05-05 20:32:59 +0300

    Outline the plan

---
+++ get_wikipedia_data.py
@@ -0,0 +1,24 @@
+#!/use/bin/env python3
+class Flower():
+    def __init__(self, name: string, active_months: list, pic_link: string):
+        self.name = name
+        self.active_months = active_months
+        self.pic_link = pic_link
+
+def get_flower_data(flower: string) -> Flower:
+    # create flower object
+    # get flower name
+    # get active months
+    # get main picture link
+    # return flower object
+
+def run_main():
+    # get flower data from WIKIPEDIA
+    # for each page in this list
+        # for each flower link
+        # flower = get_flower_data(flower: string)
+        # store flower in db
+        # grab image
+    pass
+if __name__ == '__main__':
+    run_main()
