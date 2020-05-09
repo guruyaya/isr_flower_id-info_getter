f27935627d5e865ba5d2d17ee21adeb331958f47
Yair Eshel Cahansky <guruyaya@gmail.com>
2020-05-09 18:35:11 +0300

    get google images

---
+++ get_google_images.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+from selenium import webdriver
+import time
+import urllib.parse
+
+import configparser
+from pydal import DAL
+import re
+import sys
+import sqlite3
+sys.path.append('.')
+from model import define_tables
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+db = DAL('sqlite://flower_storage.db', folder='./data')
+define_tables(db)
+
+def get_image_links(driver, search_term, count=50):
+    re_links = re.compile('(https?://[^"]*\.(jpe?g|png|gif|webp))', re.I)
+    gogole_link = "https://www.google.com/search?tbm=isch" +\
+        "&source=hp" +\
+        "&ei=2qO2XsevCseIlwSv25uIBQ" +\
+        "&q=" + urllib.parse.quote(search_term) +\
+        "&sclient=img"
+    driver.get(gogole_link)
+    source = driver.page_source
+    links = re_links.findall(source)
+    return ([link[0] for link in links])[:count]
+
+
+flowers = db().select(db.flowers.id, db.flowers.name, db.images.id.count(),
+                    groupby=db.images.flower_id,
+                    left=db.images.on(db.images.flower_id == db.flowers.id),
+                    having=(db.images.id.count() < 50)
+)
+
+max_flowers = int(config['google']['num_images'])
+
+try:
+    driver = webdriver.Firefox()
+    for flower in flowers:
+        print ("Flower name: {}".format(flower.flowers.name))
+        flower_count = flower[ db.images.id.count() ]
+
+        if flower_count >= max_flowers:
+            print ("Skipping...")
+            continue
+        links = get_image_links(driver, flower.flowers.name, count=max_flowers-flower_count)
+        for link in links:
+            try:
+                db.images.insert(url=link, flower_id=flower.flowers.id)
+            except sqlite3.IntegrityError:
+                print ("URL {} allready in db".format(link))
+            db.commit()
+finally:
+    time.sleep(5)
+    driver.close()
+    db.close()
